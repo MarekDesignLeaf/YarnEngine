af45c52b9c3ae4ef2241c1e0f31c3b38427a1387
MarekDesignLeaf <marek@designleaf.co.uk>
2026-09-24 20:13:41 +0100

    Refresh PWA cache for standalone prompt catalogues

--- src/web/static/sw.js
+++ src/web/static/sw.js
@@ -1,4 +1,4 @@
-const CACHE="opencrochet-pro-m12-v6";
+const CACHE="opencrochet-pro-m12-v7";
 const SHELL=["/static/manifest.webmanifest","/static/icon-192.png","/static/icon-512.png","/static/apple-touch-icon.png"];
 // true when this worker replaces an older one (an update), false on first install
 let isUpdate=false;
